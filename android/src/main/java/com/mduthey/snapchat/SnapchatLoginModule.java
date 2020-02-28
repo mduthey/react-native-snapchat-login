f29847c020808c5e5099ef82a956f07e1410f059
velhari <velmurugan.soft@gmail.com>
2020-02-28 18:53:14 +0530

    Android Package name changes to unique one

--- android/src/main/java/com/mduthey/snapchat/SnapchatLoginModule.java
+++ android/src/main/java/com/mduthey/snapchat/SnapchatLoginModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.mduthey.snapchat;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
--- android/src/main/java/com/mduthey/snapchat/SnapchatLoginPackage.java
+++ android/src/main/java/com/mduthey/snapchat/SnapchatLoginPackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.mduthey.snapchat;
 
 import java.util.Arrays;
 import java.util.Collections;
